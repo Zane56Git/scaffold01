68f2c7eec7a3b5e316868aef59e929dc3d631053
chenzutao <chenzutao@ly-sky.com>
2018-11-12 17:14:42 +0800

    初始化脚手架（V_init_1.0）

--- src/main/java/com/zane/scaffold/ScaffoldApplication.java
+++ src/main/java/com/zane/scaffold/ScaffoldApplication.java
@@ -1,16 +1,16 @@
 package com.zane.scaffold;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 启动类
+ * 启动类(若不配置端口，默认启动自身tomcat8080端口)
  */
 @SpringBootApplication
-@MapperScan("com.ly.skill.mapper")
 public class ScaffoldApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ScaffoldApplication.class, args);
+		System.out.println("--------------------------------您好！spring boot已经启动成功---------------------------------");
 	}
 }
---
+++ src/main/java/com/zane/scaffold/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.zane.scaffold.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Zane! This is  Spring Boot 2.0!";
+    } 
+}
