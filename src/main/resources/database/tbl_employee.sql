fb4086c31014d8e5682ffb34fba43f12cdc2f345
OS_Zane <1435865327@qq.com>
2018-11-18 23:54:11 +0800

    加入==添加mysql所用的数据库sql

---
+++ src/main/resources/database/tbl_employee.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.23-log : Database - mp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mp`;
+
+/*Table structure for table `tbl_employee` */
+
+DROP TABLE IF EXISTS `tbl_employee`;
+
+CREATE TABLE `tbl_employee` (
+  `id` varchar(32) NOT NULL,
+  `last_name` varchar(200) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `gender` varchar(100) DEFAULT NULL,
+  `age` varchar(100) DEFAULT NULL,
+  `delete_flag` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_employee` */
+
+insert  into `tbl_employee`(`id`,`last_name`,`email`,`gender`,`age`,`delete_flag`) values ('1','1','1','1','1','0');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
